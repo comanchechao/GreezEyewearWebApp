fe454b52416a238870ad5e5920a654361525a865
Greez <62405360+Wronghand-hash@users.noreply.github.com>
2022-08-27 16:50:08 +0430

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -3,7 +3,11 @@ export default function Admin() {
   return (
     <div className="h-full w-screen bg-mainYellow">
       <Navbar />
-      <div className="flex w-screen h-screen bg-mainBlue flex-col justify-center items-center"></div>
+      <div className="flex w-screen h-screen bg-mainBlue flex-col justify-center items-center">
+        <div>
+          <h2 className="text-3xl">hi Admin</h2>
+        </div>
+      </div>
     </div>
   );
 }
