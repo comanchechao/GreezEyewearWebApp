cdc74e887b16003ddabac5eb68b07887d3b6d219
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-08-28 04:34:58 +0330

    Added carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import { ForkKnife } from "phosphor-react";
 import "./App.css";
 import Navbar from "./components/navbar";
-import logo from "./assets/images/GlassesLogoBig.webp";
+import EmblaCarousel from "./components/EmblaCarousel";
 
+// import Carousel from "./components/carousel";
+// import logo from "./assets/images/GlassesLogoBig.webp";
+const SLIDE_COUNT = 5;
+const slides = Array.from(Array(SLIDE_COUNT).keys());
 function App() {
   return (
     <div className="w-screen h-full  ">
       <Navbar></Navbar>
-      <div className="flex w-screen h-screen bg-blue-400 flex-col justify-center items-center">
+      <div className="flex w-screen h-screen bg-mainWhite flex-col justify-center items-center">
         {/* <Auth></Auth> */}
 
         {/* <div className="w-3/4 h-3/5   flex items-center space-x-3 justify-center bg-mainBlue">
@@ -18,40 +22,53 @@ function App() {
           <img src={logo} alt="" />
         </div> */}
       </div>
-      <div className="flex w-screen h-screen bg-green-100 justify-around items-center">
-        <div className=" h-3/6 w-2/5 bg-mainBlue flex flex-col px-10 text-left items-start justify-center">
-          <h4 className="text-2xl">Special Offer</h4>
-          <h1
-            style={{ lineHeight: 0.8 }}
-            className="capitalize text-6xl w-72 h-auto my-1"
-          >
-            best selling men glasses
-          </h1>
-          <button
-            className="px-12 transition ease-in duration-300 hover:bg-white py-1 text-3xl my-3 bg-mainBlue outline-2 outline rounded-full outline-black"
-            type="submit"
-          >
-            Show Me
-          </button>
+      <div className="flex w-screen h-screen bg-green-100 flex-col justify-between items-center">
+        <div className="w-full h-full flex justify-around items-center">
+          <div className=" h-3/6 w-2/5 bg-mainBlue flex flex-col px-10 text-left items-start justify-center">
+            <h4 className="text-2xl">Special Offer</h4>
+            <h1
+              style={{ lineHeight: 0.8 }}
+              className="capitalize text-6xl w-72 h-auto my-1"
+            >
+              best selling men glasses
+            </h1>
+            <button
+              className="px-12 transition ease-in duration-300 hover:bg-white py-1 text-3xl my-3 bg-mainBlue outline-2 outline rounded-full outline-black"
+              type="submit"
+            >
+              Show Me
+            </button>
+          </div>
+          <div className=" h-3/6 w-2/5 bg-white flex flex-col p-10 items-start justify-center">
+            {" "}
+            <h4 className="text-2xl">Special Offer</h4>
+            <h1
+              style={{ lineHeight: 0.8 }}
+              className="capitalize text-6xl w-72 h-auto my-1"
+            >
+              best selling woman glasses
+            </h1>
+            <button
+              className="px-12 transition ease-in duration-300 hover:bg-mainBlue py-1 text-3xl my-3 bg-white outline-2 outline rounded-full outline-black"
+              type="submit"
+            >
+              Show Me
+            </button>
+          </div>
         </div>
-        <div className=" h-3/6 w-2/5 bg-white flex flex-col p-10 items-start justify-center">
-          {" "}
-          <h4 className="text-2xl">Special Offer</h4>
-          <h1
-            style={{ lineHeight: 0.8 }}
-            className="capitalize text-6xl w-72 h-auto my-1"
-          >
-            best selling woman glasses
-          </h1>
-          <button
-            className="px-12 transition ease-in duration-300 hover:bg-mainBlue py-1 text-3xl my-3 bg-white outline-2 outline rounded-full outline-black"
-            type="submit"
-          >
-            Show Me
-          </button>
+        <h1 className="text-6xl font-extrabold pb-14">Our Newest Sunnies!</h1>
+      </div>
+      <div className="flex w-screen h-auto mb-20 bg-gray-100 justify-center align-center">
+        <div className="w-full h-full  ">
+          <EmblaCarousel slides={slides} />
         </div>
       </div>
-      <div className="flex w-screen h-screen bg-mainGreen justify-center align-center"></div>
+      <div className="h-screen w-screen bg-Indigo-700 flex flex-col items-center justify-around">
+        <h1 className="text-6xl font-extrabold capitalize py-20">
+          tailor made for you
+        </h1>
+        <div className=" h-full w-screen bg-goldie"></div>
+      </div>
     </div>
   );
 }
