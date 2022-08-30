c745af5611f3369bf72306b7a4f6852efd19ce75
Greez <62405360+Wronghand-hash@users.noreply.github.com>
2022-08-30 13:25:48 +0430

    admin update

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
-import { SquaresFour, Rows } from "phosphor-react";
+import { SquaresFour, Rows, FilePlus } from "phosphor-react";
 import Navbar from "./components/navbar";
 export default function Admin() {
   const [tab, setTab] = useState("");
 
   return (
-    <div className="h-full w-screen bg-gray-100">
+    <div className="h-full overflow-x-hidden w-screen bg-mainCream">
       <Navbar />
 
       {tab !== "" && (
-        <div className="w-full flex h-48 justify-center align-center items-end">
+        <div className="w-full space-x-5 mt-8 flex h-48 justify-center align-center items-center">
           <button
             onClick={() => {
               setTab("");
             }}
-            className="px-12 transition ease-in duration-300 border-l-8 border-mainBlue hover:bg-mainBlue py-1 text-3xl my-3 bg-white outline-2 outline rounded-full outline-black"
+            className="px-12 transition ease-in duration-300 border-l-8 border-mainBlue hover:bg-mainBlue py-1 text-3xl hover:text-gray-600 font-bold my-3 bg-white outline-2 outline rounded-full outline-black"
           >
             back
           </button>
+          {tab === "blog" && (
+            <button className="px-12 transition ease-in duration-300 border-l-8 border-mainBlue hover:bg-mainBlue py-1 hover:text-gray-600 font-bold text-3xl my-3 bg-white outline-2 outline rounded-full outline-black">
+              New Blog
+            </button>
+          )}
         </div>
       )}
 
@@ -47,10 +52,10 @@ export default function Admin() {
           </div>
         </div>
       ) : tab === "sales" ? (
-        <div className="flex w-full  p-5 shadow-2xl justify-center items-center">
-          <div className="flex bg-mainWhite space-x-2  space-y-5 p-5 justify-center w-full h-full flex-row flex-wrap">
-            <div className="flex flex-col space-y-3 p-2 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-pink-400 shadoow-xl">
-              <div className="bg-gray-400 h-24 w-11/12"></div>
+        <div className="flex w-full p-4 shadow-2xl justify-center items-center">
+          <div className="flex bg-mainWhite text-gray-800 space-x-2  space-y-5 p-5 justify-center w-full h-full flex-row flex-wrap">
+            <div className="flex flex-col space-y-3 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-white shadoow-xl">
+              <div className="bg-gray-200 h-32 w-full"></div>
               <h2 className="text-3xl font-bold">Title</h2>
               <h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -58,8 +63,8 @@ export default function Admin() {
               </h3>
             </div>
 
-            <div className="flex flex-col lg:space-x-2 space-y-3 p-2 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-pink-400 shadoow-xl">
-              <div className="bg-gray-400 h-24 w-11/12"></div>
+            <div className="flex flex-col lg:space-x-2 space-y-3 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-white shadoow-xl">
+              <div className="bg-gray-200 h-32 w-full"></div>
               <h2 className="text-3xl font-bold">Title</h2>
               <h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -67,8 +72,8 @@ export default function Admin() {
               </h3>
             </div>
 
-            <div className="flex flex-col space-y-3 p-2 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-pink-400 shadoow-xl">
-              <div className="bg-gray-400 h-24 w-11/12"></div>
+            <div className="flex flex-col space-y-3 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-white shadoow-xl">
+              <div className="bg-gray-200 h-32 w-full"></div>
               <h2 className="text-3xl font-bold">Title</h2>
               <h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -76,8 +81,8 @@ export default function Admin() {
               </h3>
             </div>
 
-            <div className="flex flex-col space-y-3 p-2 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-pink-400 shadoow-xl">
-              <div className="bg-gray-400 h-24 w-11/12"></div>
+            <div className="flex flex-col space-y-3 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-white shadoow-xl">
+              <div className="bg-gray-200 h-32 w-full"></div>
               <h2 className="text-3xl font-bold">Title</h2>
               <h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -85,8 +90,8 @@ export default function Admin() {
               </h3>
             </div>
 
-            <div className="flex flex-col space-y-3 p-2 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-pink-400 shadoow-xl">
-              <div className="bg-gray-400 h-24 w-11/12"></div>
+            <div className="flex flex-col space-y-3 justify-center items-center w-full lg:w-1/3 h-52 rounded bg-white shadoow-xl">
+              <div className="bg-gray-200 h-32 w-full"></div>
               <h2 className="text-3xl font-bold">Title</h2>
               <h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -96,54 +101,57 @@ export default function Admin() {
           </div>
         </div>
       ) : tab === "blog" ? (
-        <div className="flex w-full justify-center p-5">
-          <div className="flex flex-col items-center space-y-2 w-2/3 h-full shadow-2xl rounded">
-            <div className="flex w-full items-center justify-between px-3 bg-gray-200 h-20">
-              <input
-                className="rounded"
-                placeholder="Search ... "
-                type="text"
-              />
-              <div className="flex p-2">
-                <SquaresFour className="bg-mainYellow rounded" size={32} />
-                <Rows size={32} />
-              </div>
+        <div className="flex flex-col items-center space-y-5 w-full justify-center p-5">
+          <div className="flex w-full items-center justify-between px-3 bg-gray-200 h-20">
+            <input
+              className="rounded p-3 w-80"
+              placeholder="Search ... "
+              type="text"
+            />
+            <div className="flex p-2">
+              <SquaresFour className="bg-mainYellow rounded" size={32} />
+              <Rows size={32} />
+            </div>
+          </div>
+          <div className="flex flex-col items-center space-y-2 w-full lg:w-2/3font-bold text-xl  h-full shadow-2xl rounded">
+            <div className="flex items-center  justify-around bg-mainBlue w-full h-24 rounded shadow-xl">
+              <div className="w-24 h-20 bg-gray-400 rounded-xl"></div>
+              <h2 className="text-3xl font-bold">title</h2>
+              <h2 className="text-xl">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Inventore, sit.
+              </h2>
+              <button className="transition font-bold text-xl hover:text-white bg-mainYellow p-3 rounded">show more</button>
             </div>
 
             <div className="flex items-center  justify-around bg-mainBlue w-full h-24 rounded shadow-xl">
               <div className="w-24 h-20 bg-gray-400 rounded-xl"></div>
-              <h2>title</h2>
-              <h2>
+              <h2 className="text-3xl font-bold">title</h2>
+              <h2 className="text-xl">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Inventore, sit.
               </h2>
+              <button className="transition font-bold text-xl hover:text-white bg-mainYellow p-3 rounded">show more</button>
             </div>
 
             <div className="flex items-center  justify-around bg-mainBlue w-full h-24 rounded shadow-xl">
               <div className="w-24 h-20 bg-gray-400 rounded-xl"></div>
-              <h2>title</h2>
-              <h2>
+              <h2 className="text-3xl font-bold">title</h2>
+              <h2 className="text-xl">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Inventore, sit.
               </h2>
+              <button className="transition font-bold text-xl hover:text-white bg-mainYellow p-3 rounded">show more</button>
             </div>
 
             <div className="flex items-center  justify-around bg-mainBlue w-full h-24 rounded shadow-xl">
               <div className="w-24 h-20 bg-gray-400 rounded-xl"></div>
-              <h2>title</h2>
-              <h2>
+              <h2 className="text-3xl font-bold">title</h2>
+              <h2 className="text-xl">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Inventore, sit.
               </h2>
-            </div>
-
-            <div className="flex items-center  justify-around bg-mainBlue w-full h-24 rounded shadow-xl">
-              <div className="w-24 h-20 bg-gray-400 rounded-xl"></div>
-              <h2>title</h2>
-              <h2>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Inventore, sit.
-              </h2>
+              <button className="transition font-bold text-xl hover:text-white bg-mainYellow p-3 rounded">show more</button>
             </div>
           </div>
         </div>
