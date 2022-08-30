be1c17db48646aac9117ed66f710add4a4f16f82
Greez <62405360+Wronghand-hash@users.noreply.github.com>
2022-08-30 12:59:34 +0430

    blog page update

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -1,13 +1,15 @@
 import Navbar from "./navbar";
 import nordGlasses from "../assets/images/nordGlasses.webp";
 import blogOne from "../assets/images/blogOne.webp";
-import { DotsThreeOutlineVertical } from "phosphor-react";
+import blogTwo from "../assets/images/blogTwo.webp"
+import blogThree from "../assets/images/blogThree.webp"
+import { DotsThreeOutlineVertical, SquaresFour, Rows } from "phosphor-react";
 
 export default function blogs() {
   return (
     <div className=" w-full  h-full bg-mainCream">
       <Navbar />
-      <div className="p-10 flex flex-col space-y-5 w-full h-full">
+      <div className="p-5 flex flex-col space-y-5 w-full h-full">
         <div className="mt-16 space-y-6 flex justify-end items-center flex-col  align-center">
           <div className="flex space-y-2 flex-col justify-center align-center items-center">
             <h1 className="font-bold text-3xl">Latest on eyewear Fashion</h1>
@@ -15,12 +17,17 @@ export default function blogs() {
           </div>
           <div className="flex justify-center">
             <img
-              className="object-contain w-1/2 h-1/2"
+              className="object-contain w-full h-full lg:w-1/2 lg:h-1/2"
               src={nordGlasses}
               alt=""
             />
           </div>
-          <div>
+          <div className="flex w-full justify-center items-center flex-col space-y-8">
+            <input
+              className="p-4 w-80 rounded"
+              placeholder="Search ..."
+              type="text"
+            />
             <h2 className="text-5xl font-bold">Blogs</h2>
           </div>
           <div className="bg-white p-5 rounded-full">
@@ -28,8 +35,12 @@ export default function blogs() {
           </div>
         </div>
         <div className="w-full h-full">
-          <div className="flex flex-wrap rounded space-y-2 p-4 w-full h-full  bg-mainWhite shadow-2xl">
-            <div className="flex  flex-row bg-mainCream full3 h-full">
+          <div className="flex flex-wrap rounded space-y-5 px-4 py-7 w-full h-full  bg-mainWhite shadow-2xl">
+            <div className="flex bg-white items-center rounded justify-end w-full h-12">
+              <SquaresFour className=" transition hover:bg-mainBlue" size={45} />
+              <Rows className=" transition hover:bg-mainBlue" size={45} />
+            </div>
+            <div className="flex flex-col shadow-2xl space-y-2 md:flex-row lg:flex-row bg-mainCream full3 h-full">
               <div className="w-full">
                 <img
                   src={blogOne}
@@ -37,20 +48,65 @@ export default function blogs() {
                   className=" w-full h-full object-contain"
                 />
               </div>
-              <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-2xl flex-col font-bold">Title</h1>
-                <p>sunday 2021</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <div className="flex p-2 space-y-2 flex-col justify-center items-center w-full">
+                <h1 className="text-4xl flex-col font-bold">Title</h1>
+                <p className="text-xl">sunday 2021</p>
+                <p className="text-xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </p>
               </div>
               <div className="flex justify-end p-5  items-center">
-                <button className="p-5 bg-mainBlue rounded-xl shadow-2xl">
-                  {" "}
+                <button className="font-bold hover:bg-mainYellow transition-all hover:text-white ease-in-out text-xl p-5 bg-mainBlue rounded-xl shadow-2xl">
                   Continue reading
                 </button>
               </div>
             </div>
 
-            <div className="flex flex-row bg-mainCream w-full h-full">
+            <div className="flex flex-col shadow-2xl space-y-2 md:flex-row lg:flex-row bg-mainCream w-full h-full">
+              <div className="w-full">
+                <img
+                  src={blogTwo}
+                  alt=""
+                  className=" w-full h-full object-contain"
+                />
+              </div>
+              <div className="flex p-2 space-y-2 flex-col justify-center items-center w-full">
+                <h1 className="text-4xl flex-col font-bold">Title</h1>
+                <p className="text-xl">sunday 2021</p>
+                <p className="text-xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </p>
+              </div>
+              <div className="flex justify-end p-5 items-center">
+                <button className="font-bold hover:bg-mainYellow transition-all hover:text-white ease-in-out text-xl p-5 bg-mainBlue rounded-xl shadow-2xl">
+                  Continue reading
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col shadow-2xl space-y-2 md:flex-row lg:flex-row bg-mainCream w-full h-full">
+              <div className="w-full">
+                <img
+                  src={blogThree}
+                  alt=""
+                  className=" w-full h-full object-contain"
+                />
+              </div>
+              <div className="flex p-2 space-y-2 flex-col justify-center items-center w-full">
+                <h1 className="text-4xl flex-col font-bold">Title</h1>
+                <p className="text-xl">sunday 2021</p>
+                <p className="text-xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </p>
+              </div>
+              <div className="flex justify-end p-5 items-center">
+                <button className="font-bold hover:bg-mainYellow transition-all hover:text-white ease-in-out text-xl p-5 bg-mainBlue rounded-xl shadow-2xl">
+                  Continue reading
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col shadow-2xl space-y-2 md:flex-row lg:flex-row bg-mainCream w-full h-full">
               <div className="w-full">
                 <img
                   src={blogOne}
@@ -58,20 +114,21 @@ export default function blogs() {
                   className=" w-full h-full object-contain"
                 />
               </div>
-              <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-2xl flex-col font-bold">Title</h1>
-                <p>sunday 2021</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <div className="flex p-2 space-y-2 flex-col justify-center items-center w-full">
+                <h1 className="text-4xl flex-col font-bold">Title</h1>
+                <p className="text-xl">sunday 2021</p>
+                <p className="text-xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </p>
               </div>
               <div className="flex justify-end p-5 items-center">
-                <button className="p-5 bg-mainBlue rounded-xl shadow-2xl">
-                  {" "}
+                <button className="font-bold hover:bg-mainYellow transition-all hover:text-white ease-in-out text-xl p-5 bg-mainBlue rounded-xl shadow-2xl">
                   Continue reading
                 </button>
               </div>
             </div>
 
-            <div className="flex flex-row bg-mainCream w-full h-full">
+            <div className="flex flex-col shadow-2xl space-y-2 md:flex-row lg:flex-row bg-mainCream w-full h-full">
               <div className="w-full">
                 <img
                   src={blogOne}
@@ -79,56 +136,15 @@ export default function blogs() {
                   className=" w-full h-full object-contain"
                 />
               </div>
-              <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-2xl flex-col font-bold">Title</h1>
-                <p>sunday 2021</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <div className="flex p-2 space-y-2 flex-col justify-center items-center w-full">
+                <h1 className="text-4xl flex-col font-bold">Title</h1>
+                <p className="text-xl">sunday 2021</p>
+                <p className="text-xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </p>
               </div>
               <div className="flex justify-end p-5 items-center">
-                <button className="p-5 bg-mainBlue rounded-xl shadow-2xl">
-                  {" "}
-                  Continue reading
-                </button>
-              </div>
-            </div>
-
-            <div className="flex flex-row bg-mainCream w-full h-full">
-              <div className="w-full">
-                <img
-                  src={blogOne}
-                  alt=""
-                  className=" w-full h-full object-contain"
-                />
-              </div>
-              <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-2xl flex-col font-bold">Title</h1>
-                <p>sunday 2021</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-              </div>
-              <div className="flex justify-end p-5 items-center">
-                <button className="p-5 bg-mainBlue rounded-xl shadow-2xl">
-                  {" "}
-                  Continue reading
-                </button>
-              </div>
-            </div>
-
-            <div className="flex flex-row bg-mainCream w-full h-full">
-              <div className="w-full">
-                <img
-                  src={blogOne}
-                  alt=""
-                  className=" w-full h-full object-contain"
-                />
-              </div>
-              <div className="flex flex-col justify-center items-center w-full">
-                <h1 className="text-2xl flex-col font-bold">Title</h1>
-                <p>sunday 2021</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-              </div>
-              <div className="flex justify-end p-5 items-center">
-                <button className="p-5 bg-mainBlue rounded-xl shadow-2xl">
-                  {" "}
+                <button className="font-bold hover:bg-mainYellow transition-all hover:text-white ease-in-out text-xl p-5 bg-mainBlue rounded-xl shadow-2xl">
                   Continue reading
                 </button>
               </div>
