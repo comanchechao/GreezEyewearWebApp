7c754199edcd9e94e72d1c7a75a261a9f51e87fd
Greez <62405360+Wronghand-hash@users.noreply.github.com>
2022-09-14 14:26:48 +0430

    new blog data & functions

--- src/views/newBlog.jsx
+++ src/views/newBlog.jsx
@@ -2,26 +2,116 @@ import { Upload } from "phosphor-react";
 import { useState } from "react";
 import { useRef } from "react";
 import Navbar from "../components/navbar";
+import { supabase } from "../supabaseClient";
 import { Tab } from "@headlessui/react";
 
 export default function newBlog() {
   const [section, setSection] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  const firstImageUpload = async (event) => {
+    try {
+      event.preventDefault()
+      setUploading(true);
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error("You must select an image to upload.");
+      }
+
+      const file = event.target.files[0];
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const filePath = `${fileName}`;
+      setFirstImage(filePath);
+
+      let { error: uploadError } = await supabase.storage
+        .from("blog-images")
+        .upload(filePath, file);
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      onUpload(filePath);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+      const { data, error } = await supabase.from("blogs").insert([
+        {
+          "blog-title": blogTitle,
+          "first-title": firstTitle,
+          "first-info": firstInfo,
+          "first-content": firstContent,
+          "first-image": firstImage,
+        },
+      ]);
+      if (error) throw error;
+      alert("blog added");
+    } catch (error) {
+       alert(error.error_description || error.message);
+    }
+  };
+
+  const [blogTitle, setBlogTitle] = useState("");
 
   const [firstTitle, setFirstTitle] = useState("");
+  const [firstImage, setFirstImage] = useState(null);
+  const [firstInfo, setFirstInfo] = useState("");
+  const [firstContent, setFirstContent] = useState("");
+
   const [secondTitle, setSecondTitle] = useState("");
+  const [secondInfo, setSecondInfo] = useState("");
+  const [secondContent, setSecondContent] = useState("");
+
   const [thirdTitle, setThirdTitle] = useState("");
+  const [thirdInfo, setThirdInfo] = useState("");
+  const [thirdContent, setThirdContent] = useState("");
+
   const [forthTitle, setForthTitle] = useState("");
+  const [forthInfo, setForthInfo] = useState("");
+  const [forthContent, setFourthContent] = useState("");
+
   const [fifthTItle, setFifthTitle] = useState("");
+  const [fifthInfo, setFifthInfo] = useState("");
+  const [fifthContent, setFifthContent] = useState("");
+
   const [sixthTitle, setSixthTitle] = useState("");
+  const [sixthInfo, setSixthInfo] = useState("");
+  const [sixthContent, setSixthContent] = useState("");
+
   const [seventhTitle, setSevenTitle] = useState("");
+  const [seventhInfo, setSeventhInfo] = useState("");
+  const [seventhContent, setSeventhContent] = useState("");
+
   const [eighthTitle, setEighthTitle] = useState("");
+  const [eightInfo, setEightInfo] = useState("");
+  const [eightContent, setEightContent] = useState("");
+
   const [ninethTitle, setNinethTitle] = useState("");
+  const [ninethInfo, setNinethInfo] = useState("");
+  const [ninethContent, setNinethContent] = useState("");
+
   const [tenTitle, setTenTitle] = useState("");
+  const [tenInfo, setTenInfo] = useState("");
+  const [tenContent, setTenContent] = useState("");
 
   return (
     <div className="w-screen h-full bg-CoolGray-800">
       <Navbar />
-      <div className="flex justify-center flex-col h-full w-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center flex-col h-full w-full"
+      >
         <div className="flex flex-wrap w-full h-52 justify-around mt-24 items-center">
           <label className="text-6xl text-mainWhite font-bold">
             Blog Title
@@ -29,8 +119,8 @@ export default function newBlog() {
           <input
             placeholder="type ... "
             type="text"
-            value={firstTitle}
-            onChange={(e) => setFirstTitle(e.target.value)}
+            value={blogTitle}
+            onChange={(e) => setBlogTitle(e.target.value)}
             className="py-8 w-2/3 px-4 rounded text-left text-CoolGray-900 text-xl bg-mainWhite"
           />
         </div>
@@ -43,7 +133,8 @@ export default function newBlog() {
               <Upload size={40}></Upload>
             </label>
             <input
-              placeholder="type ... "
+              onChange={firstImageUpload}
+              disabled={uploading}
               id="file"
               type="file"
               className="hidden"
@@ -55,6 +146,8 @@ export default function newBlog() {
               Enter Title
             </label>
             <input
+              value={firstTitle}
+              onChange={(e) => setFirstTitle(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -69,6 +162,8 @@ export default function newBlog() {
               more info
             </label>
             <input
+              value={firstInfo}
+              onChange={(e) => setFirstInfo(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -82,6 +177,8 @@ export default function newBlog() {
               content
             </label>
             <textarea
+              value={firstContent}
+              onChange={(e) => setFirstContent(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -96,11 +193,6 @@ export default function newBlog() {
         </div>
 
         <div className="flex space-y-6 p-5 bg-mainCream lg:mx-20 shadow-2xl rounded flex-col justify-center items-center">
-          <div className="flex justify-start p-5 items-center w-full">
-            <div className="fixed w-14 h-14 bg-Cyan-600 text-CoolGray-50 text-4xl rounded-full font-bold flex justify-center align-center items-center">
-              2
-            </div>
-          </div>
           <div className="flex space-y-4 w-full h-full flex-col justify-center items-center">
             <label
               htmlFor="file"
@@ -121,6 +213,8 @@ export default function newBlog() {
               Enter Title
             </label>
             <input
+              value={secondTitle}
+              onChange={(e) => setSecondTitle(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -135,6 +229,8 @@ export default function newBlog() {
               more info
             </label>
             <input
+              value={secondInfo}
+              onChange={(e) => setSecondInfo(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -148,6 +244,8 @@ export default function newBlog() {
               content
             </label>
             <textarea
+              value={secondContent}
+              onChange={(e) => setSecondContent(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -182,6 +280,8 @@ export default function newBlog() {
               Enter Title
             </label>
             <input
+              value={thirdTitle}
+              onChange={(e) => setThirdTitle(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -196,6 +296,8 @@ export default function newBlog() {
               more info
             </label>
             <input
+              value={thirdInfo}
+              onChange={(e) => setThirdInfo(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -209,6 +311,8 @@ export default function newBlog() {
               content
             </label>
             <textarea
+              value={thirdContent}
+              onChange={(e) => setThirdContent(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -244,6 +348,8 @@ export default function newBlog() {
               Enter Title
             </label>
             <input
+              value={forthTitle}
+              onChange={(e) => setForthTitle(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -258,6 +364,8 @@ export default function newBlog() {
               more info
             </label>
             <input
+              value={forthInfo}
+              onChange={(e) => setForthInfo(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -271,6 +379,8 @@ export default function newBlog() {
               content
             </label>
             <textarea
+              value={forthContent}
+              onChange={(e) => setForthInfo(e.target.value)}
               placeholder="type ... "
               type="text"
               id="large-input"
@@ -303,7 +413,7 @@ export default function newBlog() {
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
