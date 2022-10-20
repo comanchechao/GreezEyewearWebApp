e1c3e9d7fcf100e07a06b7938c52c8dc09b55afd
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-10-20 19:16:03 +0330

    added card picture change on hover

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,23 @@
 import cardPicture1 from "../assets/images/cardPicture1.webp";
+import cardPicture2 from "../assets/images/cardPicture2.webp";
+
 import { ShoppingCart } from "phosphor-react";
 import { Link } from "react-router-dom";
 import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import React, { Component } from "react";
 // import { RadioGroup } from "@headlessui/react";
 // import { useState } from "react";
+// let [plan, setPlan] = useState("startup");
 export default function Card() {
-  // let [plan, setPlan] = useState("startup");
   const [value, setValue] = useState("1");
   return (
     <div className="flex-col relative shadow-xl my-10 w-screen lg:w-96 h-rem26 border border-mainWhite bg-white flex justify-between pb-2 cursor-pointer items-start">
-      <img src={cardPicture1} className="object-contain w-full" alt="" />
+      <img
+        src={cardPicture1}
+        onMouseOver={(e) => (e.currentTarget.src = cardPicture2)}
+        onMouseOut={(e) => (e.currentTarget.src = cardPicture1)}
+      />
       <button
         className="transition absolute p-5  top-40 left-64 lg:left-80 ml-8 flex ease-in duration-300 border-white hover:bg-mainBlue text-mainWhite hover:text-CoolGray-900 active:bg-mainBlue active:text-CoolGray-900  text-3xl   bg-CoolGray-900  rounded-full  "
         type="submit"
