80c6fdbe01528cbe3645ceff67cbf2b2c2762914
Greez <62405360+Wronghand-hash@users.noreply.github.com>
2022-11-06 13:00:21 +0330

    debug content.jsx

--- src/components/contents.jsx
+++ src/components/contents.jsx
@@ -115,7 +115,7 @@ export default function Contents(props) {
               <input
                 type="number"
                 onChange={(e) => {
-                  dispatch(lensActions.setCylinderOS(e.target.value));
+                  dispatch(lensActions.setCylinderOD(e.target.value));
                 }}
                 className="border-gray-200 text-center rounded border-2"
                 placeholder="0.00"
@@ -155,7 +155,7 @@ export default function Contents(props) {
               <input
                 type="number"
                 onChange={(e) => {
-                  dispatch(lensActions.setSphereOS(e.target.value));
+                  dispatch(lensActions.setCylinderOS(e.target.value));
                 }}
                 className="border-gray-200 text-center rounded border-2"
                 placeholder="0.00"
